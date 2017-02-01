83466077ffe789af0f31965f3922e75c5242742f
nikoalala <nikoalala@gmail.com>
2017-02-01 21:46:39 +0100

    Ajout d'une animation karting dans le header
    L'objectif serait d'utiliser cette animation comme pointeur dans le menu
    (afin d'indiquer quelle est la section actuellement affichée).
    Je souhaite attendre de voir comment va évoluer le header (et le reste
    du site) avant.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+	//Gestion de la taille de la vidéo
     $("#videoPlayer").attr("width", getBrowserDimensions().width);
     $("#videoPlayer").attr("height", getBrowserDimensions().height-$("#headerBar").height());
 
@@ -6,4 +7,21 @@ $( document ).ready(function() {
         $("#videoPlayer").attr("width", getBrowserDimensions().width);
     	$("#videoPlayer").attr("height", getBrowserDimensions().height-$("#headerBar").height());
     });
+
+    //Gestion de l'animation du karting du header en fonction du scroll
+ 	var kartingImage = new Image();
+	kartingImage.style.position = "fixed";
+	kartingImage.style.left = "0px";
+	kartingImage.style.top = "-5px";
+	kartingImage.style["z-index"] = 1031;
+	document.body.appendChild(kartingImage);
+	kartingImage.src = "/toad/images/header/kartHeader.png";
+
+    $( window ).scroll(function() {
+    	var windowHeight = Math.max($(document).height(), $(window).height())-getBrowserDimensions().height-$("#headerBar").height();
+    	var windowWidth = getBrowserDimensions().width;
+    	var position = document.documentElement.scrollTop || document.body.scrollTop;
+
+	  	kartingImage.style.left = (position/windowHeight)*windowWidth+"px";
+	});
 });

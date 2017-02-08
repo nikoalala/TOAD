9f4f4ffa3c0ad4836a5d097d7b1f02217d41d0ec
Unknown <unknown@unknown.com>
2017-02-08 18:03:03 +0100

    [KLE] - Intégration de la video 360 dans le header - Mise a jour de la presentation et du header ( utilisation de la maquette)

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,21 +1,41 @@
 $( document ).ready(function() {
-	//Gestion de la taille de la vidéo
-    $("#videoPlayer").attr("width", getBrowserDimensions().width);
-    $("#videoPlayer").attr("height", getBrowserDimensions().height-$("#headerBar").height());
 
-    $( window ).resize(function() {
-        $("#videoPlayer").attr("width", getBrowserDimensions().width);
-    	$("#videoPlayer").attr("height", getBrowserDimensions().height-$("#headerBar").height());
-    });
+    jQuery(function($) {
 
-    //Gestion de l'animation du karting du header en fonction du scroll
+    	$('#background-video').YTPlayer({
+          fitToBackground: true,
+          videoId: 'cfX4Z2W6vX8',
+          pauseOnScroll: false,
+          callback: function() {
+            videoCallbackEvents();
+          }
+        });
+        
+        var videoCallbackEvents = function() {
+          var player = $('#background-video').data('ytPlayer').player;
+        
+          player.addEventListener('onStateChange', function(event){
+              console.log("Player State Change", event);
+
+              // OnStateChange Data
+              if (event.data === 0) {          
+                  console.log('video ended');
+              }
+              else if (event.data === 2) {          
+                console.log('paused');
+              }
+          });
+        }
+      });
+	
+    //Gestion de l'animation du karting du header en fonction du scroll 1031
  	var kartingImage = new Image();
 	kartingImage.style.position = "fixed";
 	kartingImage.style.left = "0px";
-	kartingImage.style.top = "-5px";
+	kartingImage.style.top = "4%";
 	kartingImage.style["z-index"] = 1031;
 	document.body.appendChild(kartingImage);
-	kartingImage.src = "/toad/images/header/kartHeader.png";
+	kartingImage.src = "/toad/images/header/kartHeaderNew.png";
 
     $( window ).scroll(function() {
     	var windowHeight = Math.max($(document).height(), $(window).height())-getBrowserDimensions().height-$("#headerBar").height();
